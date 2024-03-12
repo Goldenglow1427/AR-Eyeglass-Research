85f311cfb3a77e0fd28464017c84632c79a8bc24
Goldenglow1427 <magician1427@gmail.com>
2024-03-12 10:44:15 -0400

    Experimenting depth estimation by OpenCV

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 
 import cv2 as cv
+import numpy as np
+# from cv_bridge import CvBridge
+
+
+def depth_clbck(depth_msg):
+
+    # bridge = CvBridge()
+    # image = bridge.imgmsg_to_cv2(depth_msg,"32FC1")
+    image = depth_msg
+    depth_array = np.array(image, dtype=np.float32)
+    
+    # print(depth_array.shape)
+    
+    depth_colormap = cv.applyColorMap(cv.convertScaleAbs(image, alpha=0.05), cv.COLORMAP_HSV)
+
+    cv.imshow('image', np.array(depth_colormap))
+    cv.waitKey()
+
+
+# print(1)
+
+samp = cv.imread("reference_images/so_sample_image.jpg")
+# cv.imshow("image", samp)
+# cv.waitKey()
+depth_clbck(samp)
